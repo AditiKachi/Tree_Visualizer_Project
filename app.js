e97643a2a34f46fbd521f242e9b8b17683a9295b
AditiKachi <aditikachi24@gmail.com>
2022-10-23 23:22:43 +0530

    This is Tree Visualizer Project

---
+++ app.js
@@ -0,0 +1,53 @@
+let input;
+
+function reset() {
+  d3.selectAll('svg').remove();
+}
+
+function treeAndArray() {
+  reset();
+  let inputText = document.getElementById("array-input")
+  document.querySelector('#visual-title').innerHTML = "Binary Tree And Array";
+  document.querySelector('#instructions').innerHTML = "Node can have maximum 2 children.";
+  if (inputText.value !== '') {
+      input = inputText.value.trim().split(/\s+|\,+/g).map((num) => parseInt(num));
+      createBinaryTreeAndArr(input)
+  }
+}
+
+function heapify() {
+  reset();
+  let inputText = document.getElementById("array-input")
+  if (inputText.value !== '') {
+    input = inputText.value.trim().split(/\s+|\,+/g).map((num) => parseInt(num));
+    makeHeap(input, input.length);
+    createBinaryTreeAndArr(input);
+    document.getElementById('instructions').innerHTML = "<p> Parent's value is always greater than or equal to the values of its children.</p>";
+    document.getElementById('visual-title').innerHTML = "Max-Heap Binary Tree And Array";
+  }
+}
+
+function createBinaryTreeAndArr(arr) {
+  arrayContainer = createContainer("array-visual", arr, arr.length * 60, 100);
+  let tree = new Tree()
+  tree.createBinaryTree(input)
+  createArray(arr, 2, 30, 50, 50);
+}
+
+function createBinarySearchTree() {
+  let inputText = document.getElementById("array-input")
+  if (inputText.value !== '') {
+    reset();
+    input = inputText.value.trim().split(/\s+|\,+/g).map((num) => parseInt(num));
+    input.sort((a, b) => a - b);
+    document.querySelector('#visual-title').innerHTML = "Binary Search Tree";
+    document.querySelector('#instructions').innerHTML = "The input data sorted and arranged into a Binary Search Tree. The left subtree of a node contains only nodes with keys lesser than the nodes key. The right subtree of a node contains only nodes with keys greater than the nodes key.";
+    let tree = new Tree();
+    tree.createBinarySearchTree(input)
+  }
+}
+
+input = [10, 20, 60, 30, 70, 40, 50];
+let inputTest = document.getElementById("array-input")
+inputTest.value = input;
+//createBinaryTreeAndArr(input);
